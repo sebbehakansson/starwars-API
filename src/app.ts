3a63f58b890e2d0f85537d66ff98f898b70941e0
sebbehakansson <sebastian.sh74@gmail.comm>
2023-02-03 13:08:48 +0100

    big commit

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,69 @@
-const url ='https://swapi.dev/api/people/1/';
+const urlPeople ='https://swapi.dev/api/people/';
+const urlStarships ='https://swapi.dev/api/starships/';
 
-const display = document.getElementById('display') as HTMLElement;
-const categories = document.getElementById('categories') as HTMLElement;
+const people = document.getElementById('people') as HTMLElement;
+const starships = document.getElementById('starships') as HTMLElement;
+const showobject = document.getElementById('display') as HTMLElement;
+let listPeople = document.getElementById('ul-people') as HTMLElement;
+let liststarships = document.getElementById('ul-starships') as HTMLElement;
 
-async function pepole() {
-    const respons = await fetch(url);
+type characters = {
+    name: string,
+    height: string,
+    mass: string,
+    hair_color: string,
+    skin_color: string,
+    eye_color: string,
+    birth_year: string,
+    gender: string,
+    homeworld: string,
+    films: string[],
+    species: string[],
+    vehicles: string[],
+    starships: string[],
+    created: string,
+    edited: string,
+    url: string
+};
+
+let arrPeople: characters[]=[];
+
+//     let arrStarship[];
+
+async function getPepole(){
+    const respons = await fetch(urlPeople);
     const data = await respons.json();
-    display.innerHTML = data.results;
+    let person: characters;
+    for (person of data.results) {
+            let point = document.createElement('li');
+            point.innerHTML = person.name;
+            listPeople.append(point);
+            arrPeople.push(person);
+            // console.log(person);
+            point.addEventListener('click', function(event){
+                event.preventDefault;
+                let found = arrPeople.find(p => this.innerText===p.name);
+                console.log(found);
+            });
+            
+        }
 }
 
-function getPeople(pepole) {
-    for(let i = 0; i < pepole.results.lenght; i++){
-        console.log(pepole,results.lenght);
-    }
+getPepole();
+
+async function getStarships() {
+    const respons = await fetch(urlStarships);
+    const data = await respons.json();
+
+for (const starships of data.results) {
+    let point = document.createElement('li');
+    point.innerHTML = starships.name;
+    liststarships.append(point)
+    point.addEventListener('click', (event) =>{
+        event.preventDefault();
+
+    })
 }
-getPeople();
+}
+
+getStarships();
