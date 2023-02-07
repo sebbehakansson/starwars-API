1c83639d16e3c8200447a7de12ecbc788e6e19c0
sebbehakansson <sebastian.sh74@gmail.comm>
2023-02-07 19:56:56 +0100

    big commit

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 const urlPeople ='https://swapi.dev/api/people/';
 const urlStarships ='https://swapi.dev/api/starships/';
+const urlPeopleSearch = 'https://swapi.dev/api/people/?search=';
 
 const people = document.getElementById('people') as HTMLElement;
 const starships = document.getElementById('starships') as HTMLElement;
 const showobject = document.getElementById('display') as HTMLElement;
 let listPeople = document.getElementById('ul-people') as HTMLElement;
 let liststarships = document.getElementById('ul-starships') as HTMLElement;
+const inPutElement = document.querySelector('input') as HTMLInputElement;
+const buttonElement = document.querySelector('button') as HTMLButtonElement;
+const showSearch = document.getElementById('people-search') as HTMLElement;
+
 
 type characters = {
     name: string,
     height: string,
     mass: string,
-    hair_color: string,
+    hair_color: string, 
     skin_color: string,
     eye_color: string,
     birth_year: string,
@@ -28,10 +33,11 @@ type characters = {
 
 let arrPeople: characters[]=[];
 
-//     let arrStarship[];
+//let arrStarship[];
 
 async function getPepole(){
     const respons = await fetch(urlPeople);
+    
     const data = await respons.json();
     let person: characters;
     for (person of data.results) {
@@ -39,14 +45,24 @@ async function getPepole(){
             point.innerHTML = person.name;
             listPeople.append(point);
             arrPeople.push(person);
-            // console.log(person);
             point.addEventListener('click', function(event){
                 event.preventDefault;
                 let found = arrPeople.find(p => this.innerText===p.name);
-                console.log(found); 
+                if(found != undefined){
+                    showobject.innerHTML = "";
+                    let name = document.createElement('p')
+                    let height = document.createElement('p');
+                    let mass = document.createElement('p');
+                    name.innerHTML = found.name;
+                    height.innerHTML = found.height;
+                    mass.innerHTML = found.mass;
+                    showobject.append('Name:', name);
+                    showobject.append('Hight:', height);
+                    showobject.append('Mass:', mass);
+                }
             });
-            
-        }
+           
+    }
 }
 
 getPepole();
@@ -57,7 +73,7 @@ async function getStarships() {
 
 for (const starships of data.results) {
     let point = document.createElement('li');
-    point.innerHTML = starships.name;
+    point.innerHTML = starships.name; 
     liststarships.append(point)
     point.addEventListener('click', (event) =>{
         event.preventDefault();
@@ -67,3 +83,32 @@ for (const starships of data.results) {
 }
 
 getStarships();
+
+
+buttonElement.addEventListener('click', async (event) =>{
+    event.preventDefault();
+    const response = await fetch(urlPeopleSearch + inPutElement.value);
+    const data = await response.json();
+    console.log(data.results);
+    data.results.forEach((result: {
+        height: string,
+        name: string,
+        mass: string,
+        birth_year: string,
+    }) => {
+        const characterHTML = `
+        
+    <article class="char">
+    <p>Name: ${result.name}</p>
+    <p>Height: ${result.height}</p>
+    <p>Weight: ${result.mass}</p>
+    <p>Birthyear: ${result.birth_year}</p>
+    </article>`;
+
+
+    showobject.innerHTML =characterHTML;
+    });
+//}
+});
+
+    
